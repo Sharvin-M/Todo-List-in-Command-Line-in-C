af0560e8009d55ccf61a9557d8fa0a4b1c834fc9
Sharvin-M <smanjrekar2@ucmerced.edu>
2024-01-08 19:55:55 -0800

    made and tested makefile

--- main.c
+++ main.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc, char *argv[]) {
-  int taskCount = 0;
-  char buffer[256];
-  char newTask[] = "";
-  if (argc <= 0) {
-    printf("%s", "ERROR: Missing Arguement");
-    return 0;
-  } else {
-    if (strcmp(argv[2], "l")) {
-      printf("%s", "Current Tasks Todo:");
-      for (int j = 0; j < sizeof(newTask); j++) {
-        printf("%c", newTask[j]);
-      }
-    }
-    if (strcmp(argv[2], "a")) {
-      if (fgets(newTask, sizeof(buffer), stdin) != NULL) {
-        printf("%s %s", "new task added:\0", newTask);
-        taskCount++;
-      }
-      // add task
-    }
-    if (strcmp(argv[2], "d")) {
-      // delete tasks
-    }
-  }
-  return 0;
+// int main(int argc, char *argv[]) {
+
+int main(void) {
+  printf("Hello World\n");
 }
